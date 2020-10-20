0395231808ebf871efbb3c9e6dd5a4c93a8fea47
ollie <olliemullen33@gmail.com>
2020-10-20 20:56:29 +1100

    stat calculator & natural messages!

--- dndbot.js
+++ dndbot.js
@@ -7,19 +7,16 @@ const config = require("./config.json");
  * @since 16/10/2020
  */
 
- //TODO: Natural 20 message for when a 20 is rolled!
- //TODO: Fix percentile dice (d100).
-
  client.on("ready", () => {
     console.log(`Bot has been logged in as ${client.user.tag}!`);
     console.log('Ready to roll some dice!')
-    client.user.setActivity("-help", {type: 'WATCHING'});
+    client.user.setActivity(config.prefix + "help", { type: 'WATCHING' });
  });
 
 client.on("message", async message => {
   if (message.author.bot) { return; }
   if (!message.content.startsWith(config.prefix)) { return; }
-  
+
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
   
@@ -34,6 +31,7 @@ client.on("message", async message => {
         ]
       }
     });
+    if (d4roll == 4) { message.channel.send(":white_check_mark:" + " " + "**" + "Natural 4!" + "**") }
   }
 
   //d6 dice
@@ -47,6 +45,7 @@ client.on("message", async message => {
         ]
       }
     });
+    if (d6roll == 6) { message.channel.send(":white_check_mark:" + " " + "**" + "Natural 6!" + "**") }
   }
  
   //d8 dice
@@ -60,6 +59,7 @@ client.on("message", async message => {
         ]
       }
     });
+    if (d8roll == 8) { message.channel.send(":white_check_mark:" + " " + "**" + "Natural 8!" + "**") }
   }
 
   //d10 dice
@@ -73,6 +73,7 @@ client.on("message", async message => {
         ]
       }
     });
+    if (d10roll == 10) { message.channel.send(":white_check_mark:" + " " + "**" + "Natural 10!" + "**") }
   }
 
   //d12 dice
@@ -86,6 +87,7 @@ client.on("message", async message => {
         ]
       }
     });
+    if (d12roll == 12) { message.channel.send(":white_check_mark:" + " " + "**" + "Natural 12!" + "**") }
   }
 
   //d100 dice
@@ -99,6 +101,7 @@ client.on("message", async message => {
         ]
       }
     });
+    if (d100roll == 100) { message.channel.send(":white_check_mark:" + " " + "**" + "Natural 100!" + "**") }
   }
 
   //d20 dice
@@ -112,6 +115,111 @@ client.on("message", async message => {
         ]
       }
     });
+    if (d20roll == 20) { message.channel.send(":white_check_mark:" + " " + "**" + "Natural 20!" + "**") }
+  }
+
+  if (command === "statroll") {
+    //stat 1 rolling
+    var onestat1 = (Math.floor(Math.random() * 6) + 1);
+    var onestat2 = (Math.floor(Math.random() * 6) + 1);
+    var onestat3 = (Math.floor(Math.random() * 6) + 1);
+    var onestat4 = (Math.floor(Math.random() * 6) + 1);
+    //get the lowest number from the values rolled
+    const onemin = Math.min(onestat1, onestat2, onestat3, onestat4)
+    //final value
+    var onetotal = onestat1 + onestat2 + onestat3 + onestat4 - onemin
+
+    //stat 2 rolling
+    var twostat1 = (Math.floor(Math.random() * 6) + 1);
+    var twostat2 = (Math.floor(Math.random() * 6) + 1);
+    var twostat3 = (Math.floor(Math.random() * 6) + 1);
+    var twostat4 = (Math.floor(Math.random() * 6) + 1);
+    //get the lowest number from the values rolled
+    const twomin = Math.min(twostat1, twostat2, twostat3, twostat4)
+    //final value
+    var twototal = twostat1 + twostat2 + twostat3 + twostat4 - twomin
+
+    //stat 3 rolling
+    var threestat1 = (Math.floor(Math.random() * 6) + 1);
+    var threestat2 = (Math.floor(Math.random() * 6) + 1);
+    var threestat3 = (Math.floor(Math.random() * 6) + 1);
+    var threestat4 = (Math.floor(Math.random() * 6) + 1);
+    //get the lowest number from the values rolled
+    const threemin = Math.min(threestat1, threestat2, threestat3, threestat4)
+    //final value
+    var threetotal = threestat1 + threestat2 + threestat3 + threestat4 - threemin
+
+    //stat 4 rolling
+    var fourstat1 = (Math.floor(Math.random() * 6) + 1);
+    var fourstat2 = (Math.floor(Math.random() * 6) + 1);
+    var fourstat3 = (Math.floor(Math.random() * 6) + 1);
+    var fourstat4 = (Math.floor(Math.random() * 6) + 1);
+    //get the lowest number from the values rolled
+    const fourmin = Math.min(fourstat1, fourstat2, fourstat3, fourstat4)
+    //final value
+    var fourtotal = fourstat1 + fourstat2 + fourstat3 + fourstat4 - fourmin
+
+    //stat 5 rolling
+    var fivestat1 = (Math.floor(Math.random() * 6) + 1);
+    var fivestat2 = (Math.floor(Math.random() * 6) + 1);
+    var fivestat3 = (Math.floor(Math.random() * 6) + 1);
+    var fivestat4 = (Math.floor(Math.random() * 6) + 1);
+    //get the lowest number from the values rolled
+    const fivemin = Math.min(fivestat1, fivestat2, fivestat3, fivestat4)
+    //final value
+    var fivetotal = fivestat1 + fivestat2 + fivestat3 + fivestat4 - fivemin
+
+    //stat 6 rolling
+    var sixstat1 = (Math.floor(Math.random() * 6) + 1);
+    var sixstat2 = (Math.floor(Math.random() * 6) + 1);
+    var sixstat3 = (Math.floor(Math.random() * 6) + 1);
+    var sixstat4 = (Math.floor(Math.random() * 6) + 1);
+    //get the lowest number from the values rolled
+    const sixmin = Math.min(sixstat1, sixstat2, sixstat3, sixstat4)
+    //final value
+    var sixtotal = sixstat1 + sixstat2 + sixstat3 + sixstat4 - sixmin
+
+    message.channel.send({ embed: {
+        color: 3447003,
+        title: ":dragon_face:" + " " + "dndbot - stat roller",
+        fields: [{
+          //first stat
+          name: ":computer:" + " " + "Calculation 1" + " " + "(" + onestat1 + " + " + onestat2 + " + " + onestat3 + " + " + onestat4 + " - " + onemin + ")",
+          value: ":game_die:" + " " + "You rolled" + " " + "**" + onetotal + "**"
+        },
+        {
+          //second stat
+          name: ":computer:" + " " + "Calculation 2" + " " + "(" + twostat1 + " + " + twostat2 + " + " + twostat3 + " + " + twostat4 + " - " + twomin + ")",
+          value: ":game_die:" + " " + "You rolled" + " " + "**" + twototal + "**"
+        },
+        {
+          //third stat
+          name: ":computer:" + " " + "Calculation 3" + " " + "(" + threestat1 + " + " + threestat2 + " + " + threestat3 + " + " + threestat4 + " - " + threemin + ")",
+          value: ":game_die:" + " " + "You rolled" + " " + "**" + threetotal + "**"
+        },
+        {
+          //fourth stat
+          name: ":computer:" + " " + "Calculation 4" + " " + "(" + fourstat1 + " + " + fourstat2 + " + " + fourstat3 + " + " + fourstat4 + " - " + fourmin + ")",
+          value: ":game_die:" + " " + "You rolled" + " " + "**" + fourtotal + "**"
+        },
+        {
+          //fifth stat
+          name: ":computer:" + " " + "Calculation 5" + " " + "(" + fivestat1 + " + " + fivestat2 + " + " + fivestat3 + " + " + fivestat4 + " - " + fivemin + ")",
+          value: ":game_die:" + " " + "You rolled" + " " + "**" + fivetotal + "**"
+        },
+        {
+          //sixth stat
+          name: ":computer:" + " " + "Calculation 6" + " " + "(" + sixstat1 + " + " + sixstat2 + " + " + sixstat3 + " + " + sixstat4 + " - " + sixmin + ")",
+          value: ":game_die:" + " " + "You rolled" + " " + "**" + sixtotal + "**"
+        },
+        {
+          //fifth stat
+          name: ":white_check_mark:" + " " + "Done calculating!",
+          value: ":arrow_right:" + " " + "Your" + " " + "**" + "final" + "**" + " " + "values are" + " " + "**" + onetotal + "**" + "," + " " + "**" + twototal + "**" + "," + " " + "**" + threetotal + "**" + "," + " " + "**" + fourtotal + "**" + "," + " " + "**" + fivetotal + "**" + " " + "&" + " " + "**" + sixtotal + "**"
+        }
+      ],
+      }
+    });
   }
 
   //help command
@@ -120,8 +228,8 @@ client.on("message", async message => {
         color: 3447003,
         title: ":dragon_face:" + " " + "dndbot - help",
         fields: [
-          { name: "Command", value: "-d4\n-d6\n-d8\n-d10\n-d12\n-d100\n-d20\n-help", inline: true },
-          { name: "Description", value: "Rolls a 4 sided dice\nRolls a 6 sided dice\nRolls a 8 sided dice\nRolls a 10 sided dice\nRolls a 12 sided dice\nRolls a 100 sided dice\nRolls a 20 sided dice\nDisplays this page", inline: true }
+          { name: "Command", value: "-d4\n-d6\n-d8\n-d10\n-d12\n-d100\n-d20\n-help\n-statroll", inline: true },
+          { name: "Description", value: "Rolls a 4 sided dice.\nRolls a 6 sided dice.\nRolls a 8 sided dice.\nRolls a 10 sided dice.\nRolls a 12 sided dice.\nRolls a 100 sided dice.\nRolls a 20 sided dice.\nDisplays this page.\nRolls for character stats.", inline: true }
         ]
       }
     });
